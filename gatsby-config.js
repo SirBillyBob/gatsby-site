17739b6b4b4461369037acc17273b2d4326cf0e3
SirBillyBob <ahusun@andrew.cmu.edu>
2024-06-11 12:24:45 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `First Site`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
